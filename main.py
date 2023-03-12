0e77ce70986ebc56e96ae36e0073842804ae164c
Hiren Thakore <37672502+thakoreh@users.noreply.github.com>
2023-03-12 18:01:23 -0400

    Update main.py
    added makePlanWithGPT

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
+from typing import List
 import requests
 app = FastAPI()
+
+class Content(BaseModel):
+    name: str
+    desc: str
+    internaldesc: str
+
+class ItemList(BaseModel):
+    time:str
+    items: List[Content]
 
 @app.get("/{apikey}/{nextid}")
 def searchRecordsWithNextID(nextid,apikey):
@@ -18,3 +29,17 @@ def searchRecordsWithNextID(nextid,apikey):
         recordsPresent = responseJSON["records"]
         our_result_records.extend(list(filter(lambda x:float(x['NextId'])==float(nextid) and x['is_check']==True,recordsPresent)))
     return our_result_records
+
+
+
+@app.post("/askchatgpt",status_code=201)
+async def makePlanWithGPT(data:ItemList=Body(...,embed=True),status_code=201):
+    content = f"""Here are the places :\n"""
+    for d in data.items:
+        content += f"{d.name},{d.desc},{d.internaldesc}\n"
+    content += f"""I want you to plan a romantic date with my wife keeping above places in mind which starts at {data.time}. It is not necessary to include all the places. You can also come up with alternate plans."""
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "user", "content": f"{content}"}] 
+    }
+    return data
